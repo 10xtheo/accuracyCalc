871585e2d5ae79eab4e997e49a9c378d3d966a89
Theo <theoemail@internet.ru>
2023-09-21 23:55:36 +0300

    hotfixed ability to input number of measurments

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 # input section started 
-print("Введите число измерений N: ")
-N = int(input())
+N = 5
 measurments = []
-print("Введите N измерений, отправляя каждое с помощью ENTER: ")
+print("Введите 5 измерений, отправляя каждое с помощью ENTER: ")
 for i in range(N):
     measurments.append(float(input()))
 print("Введите цену деления на приборе: ")
 partition= float(input())
+# коэфициенты для 5 измерений
+coefT = 2.8 
+coefBeta = 0.51
+coefU = 0.64
+coefV = 1.67
 # input section ended
 measurments = sorted(measurments)
 N = len(measurments)
 scope = abs(measurments[4] - measurments[0]) # найдем размах
-U95coma5 = 0.64 # U коэфициент 
 # проверка на промахи
-if (abs(measurments[0] - measurments[1]) >= U95coma5*scope) or (abs(measurments[4] - measurments[3]) >= U95coma5*scope):
+if (abs(measurments[0] - measurments[1]) >= coefU*scope) or (abs(measurments[4] - measurments[3]) >= coefU*scope):
     print("Ошибка на этапе проверки на промахи")
 median = sum(measurments) / N # найдем среднее
 # найдем СКО
@@ -25,10 +28,8 @@ sqDevitation = (numerator/(N - 1))**0.5
 averageSqDevitation = sqDevitation / N**0.5 # найдем ско среднего
 
 # найдем случайную погрешность
-beta95coma5 = 0.51 # бета коэфициент
-randomAccuracyScope = beta95coma5*scope  # по размаху
-t95coma5 = 2.8 # t коэфициент
-randomAccuracyAverageSqDevitation = t95coma5*averageSqDevitation # по ско
+randomAccuracyScope = coefBeta*scope  # по размаху
+randomAccuracyAverageSqDevitation = coefT*averageSqDevitation # по ско
 randomAccuracy = max(randomAccuracyAverageSqDevitation, randomAccuracyScope) # выберем наибольшую случайную погрешность
 instrumentAccuracy = partition/2 # найдем приборную погрешность
 fullAccuracy = (randomAccuracy**2 + instrumentAccuracy**2)**0.5 # найдем полную погрешность
